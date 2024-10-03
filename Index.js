fd9666a5a36908d13570493c7a1ac9905aecf5ed
Nasim6337 <163528075+Nasim6337@users.noreply.github.com>
2024-10-03 10:41:48 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,23 @@
+const express= require('express')
 
+const cors=require('cors')
+
+const app= express();
+
+const dotenv=require('dotenv')
+
+dotenv.config();
+
+const locationData= require('./location.json')
+
+app.use(cors())
+
+app.get('/api/location',(req,res)=>{
+
+const data = locationData;
+
+res.json(data);
+
+});
+
+app.listen(process.env.PORT||3000);
